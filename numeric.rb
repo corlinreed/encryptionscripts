51e1b286f0f2c527e9d32540fd3a57482d172ce5
Corlin Reed <corlinreed@gmail.com>
2019-03-11 21:44:28 -0600

    fix numeric script

--- numeric.rb
+++ numeric.rb
@@ -3,27 +3,27 @@ puts "Numeric Encrytpion/Decryption"
 print "Encrypt (e) / decrypt (d)? "
 choice = gets
 if choice.chomp == "e"
-	print "Enter text to encrypt: "
-	plaintext = gets
-	ciphertext = plaintext.chomp!.chars
-	ciphertext.each do |letter|
-		letter = letter.ord
-    		if (64..90).include? letter #check if it's a capital letter
-			letter = letter - 64
-			letter = 27 - letter
-    		elsif (97..122).include? letter #check if it's a lowercase letter
-			letter = letter - 97
-			letter = 27 - letter
-		elsif letter == 59 #check if it's a space
-			letter = ' '
-		end
-		print letter.to_s + ' '
-	end
-	print "\n"
-	exit
-elsif choice.chomp == 'd' 
-	print "Enter text to decrypt (numbers separated by spaces): "
+        print "Enter text to encrypt: "
+        plaintext = gets
+        ciphertext = plaintext.chomp!.chars
+        ciphertext.each do |letter|
+                letter = letter.ord
+                if (64..90).include? letter #check if it's a capital letter
+                        letter = letter - 64
+                        letter = 27 - letter
+                elsif (97..122).include? letter #check if it's a lowercase letter
+                        letter = letter - 97
+                        letter = 27 - letter
+                elsif letter == 59 #check if it's a space
+                        letter = ' '
+                end
+                print letter.to_s + ' '
+        end
+        print "\n"
+        exit
+elsif choice.chomp == 'd'
+        print "Enter text to decrypt (numbers separated by spaces): "
 else
-	puts "Invalid choice."
-	exit
+        puts "Invalid choice."
+        exit
 end
